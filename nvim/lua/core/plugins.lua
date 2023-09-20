00d97c330635f7217dd8b8edf00953681e98c3a9
unholysweater <15268681+unholysweater@users.noreply.github.com>
2023-09-20 14:42:52 -0400

    Update plugins.lua
    ORG MODE BACK ON THE MENU BABY

--- nvim/lua/core/plugins.lua
+++ nvim/lua/core/plugins.lua
@@ -37,4 +37,32 @@ require('lazy').setup({
     build = function() vim.fn["mkdp#util#install"]() end,
   },
 
+  {
+  'nvim-orgmode/orgmode',
+  dependencies = {
+    { 'nvim-treesitter/nvim-treesitter', lazy = true },
+  },
+  event = 'VeryLazy',
+  config = function()
+    -- Load treesitter grammar for org
+    require('orgmode').setup_ts_grammar()
+
+    -- Setup treesitter
+    require('nvim-treesitter.configs').setup({
+      highlight = {
+        enable = true,
+        additional_vim_regex_highlighting = { 'org' },
+      },
+      ensure_installed = { 'org' },
+    })
+
+    -- Setup orgmode
+    require('orgmode').setup({
+      org_agenda_files = '~/orgfiles/**/*',
+      -- Update if necessary if you don't want to use refile
+      org_default_notes_file = '~/orgfiles/refile.org',
+    })
+  end,
+},
+    
   })
