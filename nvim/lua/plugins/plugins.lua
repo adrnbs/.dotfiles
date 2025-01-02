1d94ad5592a539d76eba76f878180e1bb616cb10
mainframegremlin <15268681+mainframegremlin@users.noreply.github.com>
2025-01-02 09:51:15 -0500

    Update plugins.lua

--- nvim/lua/plugins/plugins.lua
+++ nvim/lua/plugins/plugins.lua
@@ -6,32 +6,20 @@ return {
   { "nvim-tree/nvim-web-devicons" },
   { "christoomey/vim-tmux-navigator" },
 
-  -- add dracula
-  --{ "Mofiqul/dracula.nvim" },
-
-    {
-      'sainnhe/gruvbox-material',
-      lazy = false,
-      priority = 1000,
-      config = function()
-        -- Optionally configure and load the colorscheme
-        -- directly inside the plugin declaration.
-        vim.g.gruvbox_material_enable_italic = true
-        vim.cmd.colorscheme('gruvbox-material')
-      end
-    },
-
-  -- Configure LazyVim to load dracula
-  -- {
-  --   "LazyVim/LazyVim",
-  --   opts = {
-  --     colorscheme = "",
-  --   },
-  -- },
+  {
+    "Mofiqul/dracula.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.cmd([[colorscheme dracula]])
+    end,
+  },
 
   {
-      'nvim-treesitter/nvim-treesitter',
-      build = function() require('nvim-treesitter.install').update({ with_sync = true }) end,
+    "nvim-treesitter/nvim-treesitter",
+    build = function()
+      require("nvim-treesitter.install").update({ with_sync = true })
+    end,
   },
 
   {
@@ -39,10 +27,9 @@ return {
     event = { "VeryLazy" },
     build = "deno task --quiet build:fast",
     config = function()
-        require("peek").setup()
-        vim.api.nvim_create_user_command("PeekOpen", require("peek").open, {})
-        vim.api.nvim_create_user_command("PeekClose", require("peek").close, {})
+      require("peek").setup()
+      vim.api.nvim_create_user_command("PeekOpen", require("peek").open, {})
+      vim.api.nvim_create_user_command("PeekClose", require("peek").close, {})
     end,
   },
-
-  }
+}
